3c95c08e52fed06d9922cd00904e5820c37910dc
Sarva Dubey <sarvadubey@gmail.com>
2025-09-26 18:39:18 +0530

    Added Database

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from fastapi import FastAPI, HTTPException            #IMPORT
-from schemas import GenreURLChoices, BandBase, BandWithID, BandCreate
+from fastapi import FastAPI, HTTPException, Depends, Query            #IMPORT
+from models import GenreURLChoices, Band, BandCreate, Album
+from db import init_db, get_session
+from contextlib import asynccontextmanager
+from sqlmodel import Session, select
+from typing import Annotated
 
-app = FastAPI() #INSTANCE OF FASTAPI
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+   init_db()
+   yield
+
+app = FastAPI(lifespan=lifespan) #INSTANCE OF FASTAPI
 
 #@app.get('/') #ROUTE
 #async def index() -> dict[str, str]: #ASYNC FUNCTION WITH PROPER RETURN TYPE
@@ -20,44 +29,57 @@ app = FastAPI() #INSTANCE OF FASTAPI
 
 
 #Demo data in format --> list[dict{str, str}]
+
+'''
 BANDS = [       
      {'id': 1, 'name': 'The Kinks', 'genre': 'Rock'},
      {'id': 2, 'name': 'Aphex Twin', 'genre': 'Electronic' },
          {'id': 3, 'name': 'Black Sabbath', 'genre': 'Metal', 'albums': [{'title': 'Master of Reality', 'release_date': '1971-07-21'}]},
      {'id': 4, 'name': 'Wu-Tang Clan', 'genre': 'Hip-Hop'},
 ]
-
+'''
 @app.get('/bands')
-async def bands(genre: GenreURLChoices | None = None, has_albums: bool = False) -> list[BandBase]:
-    band_list = [BandBase(**b) for b in BANDS]   #Framing the data from BANDS in pydantic schema
+async def bands(genre: GenreURLChoices | None = None, 
+                q: Annotated[str | None, Query(max_length=10)] = None,
+                has_albums: bool = False, session: Session = Depends(get_session)
+                ) -> list[Band]:
+    band_list = session.exec(select(Band)).all()   
     if genre:
-        band_list = [b for b in band_list if b.genre.lower() == genre.value]
-
-    if has_albums:
-        band_list = [b for b in band_list if len(b.albums) > 0]
-    
+        band_list = [b for b in band_list if b.genre.value.lower() == genre.value]
+    if q:
+        band_list = [b for b in band_list if q.lower() in b.name.lower()]    
     return band_list
 
-
 @app.get('/bands/{band_id}') #ROUTE
-async def band(band_id: int) -> BandWithID:   #Return format is a obj under Band class(schemas)
-  band = next((BandWithID(**b) for b in BANDS if b['id'] == band_id), None)
+async def band(band_id: int,
+               session: Session = Depends(get_session)               
+               ) -> Band:   
+  band = session.get(Band, band_id)
   if band is None: 
     raise HTTPException(status_code = 404, detail = 'Band not found')
   return band
-
-
-@app.get('/bands/genre/{genre}')
-async def bands_for_genre(genre: GenreURLChoices) -> list[dict]:  #NOT USING PYDANTIC HERE
-    return[
-      b for b in BANDS if b['genre'].lower() == genre.value
-    ]
+ 
+# @app.get('/bands/genre/{genre}')
+# async def bands_for_genre(genre: GenreURLChoices) -> list[dict]:  #NOT USING PYDANTIC HERE
+#     return[
+#       b for b in BANDS if b['genre'].lower() == genre.value
+#     ]
 
 
 @app.post('/bands')
-async def create_bands(band_create: BandCreate) -> BandWithID:
-   id = BANDS[-1]['id'] + 1   #Creating an id for newly added band by the user
-   Band = BandWithID(id = id , **band_create.model_dump()).model_dump() #Dumping into dict and variable
-   BANDS.append(Band)  # Add into list
-   return Band
-   
+async def create_bands(
+   band_data: BandCreate,
+   session: Session = Depends(get_session)
+   )-> Band:
+   band = Band(name=band_data.name, genre=band_data.genre)
+   session.add(band)
+
+   if band_data.albums:
+      for album in band_data.albums:
+         album_obj = Album(title=album.title, release_date=album.release_date, band=band)
+         session.add(album_obj)       
+
+   session.commit()
+   session.refresh(band)
+   return band
+  
